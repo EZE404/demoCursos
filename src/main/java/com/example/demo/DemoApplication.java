d5367329095a7b25b29877b62bcf3d617276de58
Franco Ezequiel <70961512+EZE404@users.noreply.github.com>
2023-07-06 14:03:01 -0300

    ordenando un poco el main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -49,68 +49,9 @@ public class DemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
 		return args -> {
-			// CURSO
-			Curso unCurso = new Curso(
-					null,
-					"SpringBoot",
-					"Curso para aprender a levantar un API Rest",
-					LocalDate.of(2019, Month.APRIL, 1),
-					LocalDate.of(2023, Month.DECEMBER, 10));
+			// CARGAR DATOS DE PRUEBA
 
-			Curso otroCurso = new Curso(
-					null,
-					"React",
-					"Curso de React Native",
-					LocalDate.now(),
-					LocalDate.of(2025, 1, 12));
-
-			unCurso = cursoRepo.saveAndFlush(unCurso);
-			otroCurso = cursoRepo.saveAndFlush(otroCurso);
-
-			// ESTUDIANTE
-
-			Estudiante franco = new Estudiante(null,
-					"Franco", "Albornoz", "11222333",
-					"eze@correo", LocalDate.of(2010, Month.SEPTEMBER, 16));
-
-			Estudiante pablo = new Estudiante(null,
-					"Pablo", "Cabrera", "11222444",
-					"pablo@correo", LocalDate.of(1989, Month.JUNE, 19));
-			estudianteRepo.save(franco);
-
-			Estudiante lauta = new Estudiante(null,
-					"Lautaro", "Romero", "55777333",
-					"lau@correo", LocalDate.of(1995, Month.MAY, 8));
-
-			Estudiante sofia = new Estudiante(null,
-					"Sofía", "Cabrera", "77222333",
-					"sofi@correo", LocalDate.of(1985, Month.DECEMBER, 23));
-
-			franco = estudianteRepo.saveAndFlush(franco);
-			pablo = estudianteRepo.saveAndFlush(pablo);
-			lauta = estudianteRepo.saveAndFlush(lauta);
-			sofia = estudianteRepo.saveAndFlush(sofia);
-
-			// INSCRIPCIÓN
-			Inscripcion inscripcion1 = new Inscripcion(null, unCurso, franco, LocalDate.now(),
-					Inscripcion.Estado.PENDIENTE);
-			Inscripcion inscripcion2 = new Inscripcion(null, otroCurso, franco, LocalDate.now(),
-					Inscripcion.Estado.ACEPTADA);
-			Inscripcion inscripcion3 = new Inscripcion(null, unCurso, pablo, LocalDate.now(),
-					Inscripcion.Estado.RECHAZADA);
-			Inscripcion inscripcion4 = new Inscripcion(null, unCurso, lauta, LocalDate.now(),
-					Inscripcion.Estado.PENDIENTE);
-			Inscripcion inscripcion5 = new Inscripcion(null, unCurso, sofia, LocalDate.now(),
-					Inscripcion.Estado.RECHAZADA);
-			Inscripcion inscripcion6 = new Inscripcion(null, otroCurso, sofia, LocalDate.now(),
-					Inscripcion.Estado.ACEPTADA);
-
-			inscripcionRepo.save(inscripcion1);
-			inscripcionRepo.save(inscripcion2);
-			inscripcionRepo.save(inscripcion3);
-			inscripcionRepo.save(inscripcion4);
-			inscripcionRepo.save(inscripcion5);
-			inscripcionRepo.save(inscripcion6);
+			cargarDatosPrueba();
 
 			// PROBANDO SERVICES Y CONSULTAS
 
@@ -129,5 +70,70 @@ public class DemoApplication {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
+
+	void cargarDatosPrueba() {
+		// CURSO
+		Curso unCurso = new Curso(
+				null,
+				"SpringBoot",
+				"Curso para aprender a levantar un API Rest",
+				LocalDate.of(2019, Month.APRIL, 1),
+				LocalDate.of(2023, Month.DECEMBER, 10));
+
+		Curso otroCurso = new Curso(
+				null,
+				"React",
+				"Curso de React Native",
+				LocalDate.now(),
+				LocalDate.of(2025, 1, 12));
+
+		unCurso = cursoRepo.saveAndFlush(unCurso);
+		otroCurso = cursoRepo.saveAndFlush(otroCurso);
+
+		// ESTUDIANTE
+
+		Estudiante franco = new Estudiante(null,
+				"Franco", "Albornoz", "11222333",
+				"eze@correo", LocalDate.of(2010, Month.SEPTEMBER, 16));
+
+		Estudiante pablo = new Estudiante(null,
+				"Pablo", "Cabrera", "11222444",
+				"pablo@correo", LocalDate.of(1989, Month.JUNE, 19));
+		estudianteRepo.save(franco);
+
+		Estudiante lauta = new Estudiante(null,
+				"Lautaro", "Romero", "55777333",
+				"lau@correo", LocalDate.of(1995, Month.MAY, 8));
+
+		Estudiante sofia = new Estudiante(null,
+				"Sofía", "Cabrera", "77222333",
+				"sofi@correo", LocalDate.of(1985, Month.DECEMBER, 23));
+
+		franco = estudianteRepo.saveAndFlush(franco);
+		pablo = estudianteRepo.saveAndFlush(pablo);
+		lauta = estudianteRepo.saveAndFlush(lauta);
+		sofia = estudianteRepo.saveAndFlush(sofia);
+
+		// INSCRIPCIÓN
+		Inscripcion inscripcion1 = new Inscripcion(null, unCurso, franco, LocalDate.now(),
+				Inscripcion.Estado.PENDIENTE);
+		Inscripcion inscripcion2 = new Inscripcion(null, otroCurso, franco, LocalDate.now(),
+				Inscripcion.Estado.ACEPTADA);
+		Inscripcion inscripcion3 = new Inscripcion(null, unCurso, pablo, LocalDate.now(),
+				Inscripcion.Estado.RECHAZADA);
+		Inscripcion inscripcion4 = new Inscripcion(null, unCurso, lauta, LocalDate.now(),
+				Inscripcion.Estado.PENDIENTE);
+		Inscripcion inscripcion5 = new Inscripcion(null, unCurso, sofia, LocalDate.now(),
+				Inscripcion.Estado.RECHAZADA);
+		Inscripcion inscripcion6 = new Inscripcion(null, otroCurso, sofia, LocalDate.now(),
+				Inscripcion.Estado.ACEPTADA);
+
+		inscripcionRepo.save(inscripcion1);
+		inscripcionRepo.save(inscripcion2);
+		inscripcionRepo.save(inscripcion3);
+		inscripcionRepo.save(inscripcion4);
+		inscripcionRepo.save(inscripcion5);
+		inscripcionRepo.save(inscripcion6);
 	}
 }
